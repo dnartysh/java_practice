eec34c0b20f09d5fc40cc0eef0a2346cad82f799
Denis <dnartysh@yandex.ru>
2020-07-16 23:56:16 +0300

    Lesson 4.1 - sumDigits

--- 04_NumbersStringsAndDates/IntegerExperiments/src/Container.java
+++ 04_NumbersStringsAndDates/IntegerExperiments/src/Container.java
@@ -1,4 +1,4 @@
 public class Container
 {
-    public Integer count;
+    public int count;
 }
--- 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
+++ 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
@@ -1,15 +1,31 @@
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
         Container container = new Container();
         container.count += 7843;
-
+        givenSumNumbers();
     }
 
-    public Integer sumDigits(Integer number)
+    public static void givenSumNumbers()
     {
-        //@TODO: write code here
-        return 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        System.out.println("Sum numbers: " + sumDigits(scanner.nextInt()));
+    }
+
+    public static Integer sumDigits(Integer number)
+    {
+        int sumNumbers = 0;
+
+        while (number != 0)
+        {
+            sumNumbers += number % 10;
+            number /= 10;
+        }
+
+        return sumNumbers;
     }
 }
