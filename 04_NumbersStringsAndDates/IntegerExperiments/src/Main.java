cbfbccd524a61831f68fb6ccc680cef02ecbf132
Denis <dnartysh@yandex.ru>
2020-07-18 21:03:36 +0300

    Lesson 4.1 - getNumericValue and valueOf

--- 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
+++ 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
@@ -14,16 +14,31 @@ public class Main
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter number: ");
         System.out.println("Sum numbers: " + sumDigits(scanner.nextInt()));
+        System.out.print("Enter number for variant 2: ");
+        System.out.println("Sum numbers: " + sumDigitsVersion2(scanner.nextInt()));
     }
 
     public static Integer sumDigits(Integer number)
     {
         int sumNumbers = 0;
 
-        while (number != 0)
-        {
-            sumNumbers += number % 10;
-            number /= 10;
+        String someText = Integer.toString(number);
+
+        for (int i = 0; i <= someText.length() - 1; i++) {
+            sumNumbers += Integer.parseInt(String.valueOf(someText.charAt(i)));
+        }
+
+        return sumNumbers;
+    }
+
+    public static Integer sumDigitsVersion2(Integer number)
+    {
+        int sumNumbers = 0;
+
+        String someText = Integer.toString(number);
+
+        for (int i = 0; i <= someText.length() - 1; i++) {
+            sumNumbers += Character.getNumericValue(someText.charAt(i));
         }
 
         return sumNumbers;
