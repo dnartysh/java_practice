69519d884a94b8fac7899cfacda175c663b517ae
Denis <dnartysh@yandex.ru>
2020-07-18 21:42:33 +0300

    Lesson 4.2 - Min and max value

---
+++ 04_NumbersStringsAndDates/MinAndMaxValue/src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static void main(String[] args)
+    {
+
+        double valueDoubleMin = Double.MIN_VALUE;
+        double valueDoubleMax = Double.MAX_VALUE;
+
+        float valueFloatMin = Float.MIN_VALUE;
+        float valueFloatMax = Float.MAX_VALUE;
+
+        byte valueByteMin = Byte.MIN_VALUE;
+        byte valueByteMax = Byte.MAX_VALUE;
+
+        int valueIntMin = Integer.MIN_VALUE;
+        int valueIntMax = Integer.MAX_VALUE;
+
+        short valueShortMin = Short.MIN_VALUE;
+        short valueShortMax = Short.MAX_VALUE;
+
+        long valueLongMin = Long.MIN_VALUE;
+        long valueLongMax = Long.MAX_VALUE;
+
+        // Double
+        System.out.println("Min value double: " + valueDoubleMin + " \nMax value double: " + valueDoubleMax + "\n");
+
+        // Float
+        System.out.println("Min value float: " + valueFloatMin + " \nMax value float: " + valueFloatMax + "\n");
+
+        // Byte
+        System.out.println("Min value byte: " + valueByteMin + " \nMax value byte: " + valueByteMax + "\n");
+
+        // Int
+        System.out.println("Min value int: " + valueIntMin + " \nMax value int: " + valueIntMax + "\n");
+
+        // Short
+        System.out.println("Min value short: " + valueShortMin + " \nMax value short: " + valueShortMax + "\n");
+
+        // Long
+        System.out.println("Min value long: " + valueLongMin + " \nMax value long: " + valueLongMax + "\n");
+    }
+}
