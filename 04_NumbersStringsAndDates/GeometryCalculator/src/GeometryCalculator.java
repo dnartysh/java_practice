77adf2eb99d0007d334b19114bdd56aac512ddf3
Denis <dnartysh@yandex.ru>
2020-07-20 21:23:30 +0300

    Lesson 4.3 - squareCircle, volumeSphere, Math

--- 04_NumbersStringsAndDates/GeometryCalculator/src/GeometryCalculator.java
+++ 04_NumbersStringsAndDates/GeometryCalculator/src/GeometryCalculator.java
@@ -1,21 +1,28 @@
 public class GeometryCalculator {
     // метод должен использовать абсолютное значение radius
     public static double getCircleSquare(double radius) {
-        return 0;
+        return Math.PI * Math.pow(radius, 2.0);
     }
 
     // метод должен использовать абсолютное значение radius
     public static double getSphereVolume(double radius) {
-        return 0;
+        return  4 * Math.PI * Math.pow(radius, 2.0);
     }
 
     public static boolean isTriangleRightAngled(double a, double b, double c) {
-        return false;
+        return (a + b) > c || (a + c) > b || (b + c) > a;
     }
 
     // перед расчетом площади рекомендуется проверить возможен ли такой треугольник
     // методом isTriangleRightAngled, если невозможен вернуть -1.0
     public static double getTriangleSquare(double a, double b, double c) {
-        return 0;
+        if (isTriangleRightAngled(a, b, c)) {
+            double p = (a + b + c) / 2;
+            return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+        else {
+            return 0.0;
+        }
+
     }
 }
