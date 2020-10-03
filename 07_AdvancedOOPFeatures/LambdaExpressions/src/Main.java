f66c725adb53ffe6d9a44a6c07744348ad767954
Denis <dnartysh@yandex.ru>
2020-10-04 01:15:51 +0300

    Lesson 7.1 - sorting list staff, add comparing, thenComparing

--- 07_AdvancedOOPFeatures/LambdaExpressions/src/Main.java
+++ 07_AdvancedOOPFeatures/LambdaExpressions/src/Main.java
@@ -5,45 +5,47 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.stream.Stream;
 
-public class Main
-{
+public class Main {
+
     private static String staffFile = "data/staff.txt";
     private static String dateFormat = "dd.MM.yyyy";
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         ArrayList<Employee> staff = loadStaffFromFile();
         staff.sort(((o1, o2) -> {
-            return o1.getSalary().compareTo(o2.getSalary() +
-                    o2.getName().compareTo(o1.getName()));
+            if (o1.getSalary().compareTo(o2.getSalary()) == 0) {
+                return o1.getName().compareTo(o2.getName());
+            } else {
+                return o1.getSalary().compareTo(o2.getSalary());
+            }
         }));
+
+        staff.sort(Comparator.comparing(Employee::getSalary).thenComparing(Employee::getName));
 
         for (Employee employee : staff) {
             System.out.println(employee);
         }
     }
 
-    private static ArrayList<Employee> loadStaffFromFile()
-    {
+    private static ArrayList<Employee> loadStaffFromFile() {
         ArrayList<Employee> staff = new ArrayList<>();
         try {
             List<String> lines = Files.readAllLines(Paths.get(staffFile));
-            for(String line : lines) {
+            for (String line : lines) {
                 String[] fragments = line.split("\t");
 
-                if(fragments.length != 3) {
+                if (fragments.length != 3) {
                     System.out.println("Wrong line: " + line);
                     continue;
                 }
 
                 staff.add(new Employee(
-                    fragments[0],
-                    Integer.parseInt(fragments[1]),
-                    (new SimpleDateFormat(dateFormat)).parse(fragments[2])
+                        fragments[0],
+                        Integer.parseInt(fragments[1]),
+                        (new SimpleDateFormat(dateFormat)).parse(fragments[2])
                 ));
             }
-        }
-        catch (Exception ex) {
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
         return staff;
