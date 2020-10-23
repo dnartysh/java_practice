1b979a089783f4f93910462c700c53712e1509a3
Denis <dnartysh@yandex.ru>
2020-10-23 20:04:24 +0300

    Lesson 8.1 - Exceptions

--- 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/Customer.java
+++ 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/Customer.java
@@ -1,18 +1,15 @@
-public class Customer
-{
+public class Customer {
     private String name;
     private String phone;
     private String eMail;
 
-    public Customer(String name, String phone, String eMail)
-    {
+    public Customer(String name, String phone, String eMail) {
         this.name = name;
         this.phone = phone;
         this.eMail = eMail;
     }
 
-    public String toString()
-    {
+    public String toString() {
         return name + " - " + eMail + " - " + phone;
     }
 }
--- 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
+++ 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
@@ -1,33 +1,38 @@
 import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class CustomerStorage
-{
+public class CustomerStorage {
     private HashMap<String, Customer> storage;
 
-    public CustomerStorage()
-    {
+    public CustomerStorage() {
         storage = new HashMap<>();
     }
 
-    public void addCustomer(String data)
-    {
+    public void addCustomer(String data) {
         String[] components = data.split("\\s+");
-        String name = components[0] + " " + components[1];
-        storage.put(name, new Customer(name, components[3], components[2]));
+        if (components.length != 4) {
+            throw new IllegalArgumentException("Неверно введен запрос. Пример запроса:\n"
+                    + "add Василий Петров vasily.petrov@gmail.com +79215637722");
+        } else if (!Pattern.matches(".+@.+\\..+", components[2])) {
+            throw new IllegalArgumentException("Неверно введен e-mail.");
+        } else if (!Pattern.matches("\\+7.{10}", components[3])) {
+            throw new IllegalArgumentException("Неверно введен номер телефона.");
+        } else {
+            String name = components[0] + " " + components[1];
+            storage.put(name, new Customer(name, components[3], components[2]));
+        }
     }
 
-    public void listCustomers()
-    {
+    public void listCustomers() {
         storage.values().forEach(System.out::println);
     }
 
-    public void removeCustomer(String name)
-    {
+    public void removeCustomer(String name) {
         storage.remove(name);
     }
 
-    public int getCount()
-    {
+    public int getCount() {
         return storage.size();
     }
 }
