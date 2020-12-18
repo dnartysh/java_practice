a7c0308c5db30926bcbb96cc08e37284ae6fc521
Denis <dnartysh@yandex.ru>
2020-12-18 14:12:20 +0300

    Lesson 9.1 - small changes. Add new method Files.walkFileTree()

--- 09_FilesAndNetwork/FoldersAndFilesSizes/src/Main.java
+++ 09_FilesAndNetwork/FoldersAndFilesSizes/src/Main.java
@@ -1,15 +1,20 @@
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Scanner;
 
 public class Main {
 
+    private final static int KB_IN_B = 1024;
     private final static int KB_IN_MB = 1024000;
     private final static int KB_IN_GB = 1048576000;
 
     private static Double filesSize;
-    private static List<File> nestedFolders = new ArrayList<>();
 
     public static void main(String[] args) {
         while (true) {
@@ -24,7 +29,14 @@ public class Main {
             File file = new File(path);
 
             if (file.isDirectory()) {
-                getNestedFiles(file);
+                Files.walkFileTree(Paths.get(path), new SimpleFileVisitor<>() {
+                    @Override
+                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+                            throws IOException {
+                        filesSize += attrs.size();
+                        return super.visitFile(file, attrs);
+                    }
+                });
                 printFilesSize("Размер выбранной папки " + path + " составляет - ");
             } else {
                 calculateFilesSize(file);
@@ -33,37 +45,6 @@ public class Main {
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-    }
-
-    private static void getNestedFiles(File file) {
-        for (File nestedFile : file.listFiles()) {
-            if (nestedFile.isFile()) {
-                calculateFilesSize(nestedFile);
-            } else {
-                nestedFolders.add(nestedFile);
-            }
-        }
-
-        while (nestedFolders.size() != 0) {
-            getNestedFolders(nestedFolders);
-        }
-    }
-
-    private static void getNestedFolders(List<File> folders) {
-        List<File> bufferFolders = new ArrayList<>();
-
-        for (File nestedFolder : folders) {
-            for (File nestedFile : nestedFolder.listFiles()) {
-                if (nestedFile.isFile()) {
-                    calculateFilesSize(nestedFile);
-                } else {
-                    bufferFolders.add(nestedFile);
-                }
-            }
-        }
-
-        nestedFolders.clear();
-        nestedFolders.addAll(bufferFolders);
     }
 
     private static void calculateFilesSize(File file) {
@@ -84,12 +65,12 @@ public class Main {
     private static String getFormattedSize(Double size) {
         int length = String.valueOf(size).length();
 
-        if (length <= 12) {
-            return String.format("%.2f", size / KB_IN_MB) + " mb";
-        } else if (length > 13) {
-            return String.format("%.2f" ,size / KB_IN_GB) + " gb";
+        if (length > 8  && length <= 12) {
+            return String.format("%.2f", size / KB_IN_MB) + " Mb";
+        } else if (length >= 13) {
+            return String.format("%.2f", size / KB_IN_GB) + " Gb";
         }
 
-        return size + " kb";
+        return String.format("%.2f", size / KB_IN_B) + " Kb";
     }
 }
