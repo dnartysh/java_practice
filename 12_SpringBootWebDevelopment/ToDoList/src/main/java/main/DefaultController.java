e03f1a8cee64d9b1e232cce356a2d4a97d28c7f4
Denis <dnartysh@yandex.ru>
2021-03-13 12:05:37 +0300

    Lesson 12.1 - new project, add DefaultController

---
+++ 12_SpringBootWebDevelopment/ToDoList/src/main/java/main/DefaultController.java
@@ -0,0 +1,14 @@
+package main;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DefaultController {
+
+    @RequestMapping("/")
+    public double getDate() {
+        return Math.random();
+    }
+
+}
